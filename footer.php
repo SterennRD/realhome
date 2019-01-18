d02d757f7493edda46d2e6da534a66224fed3b34
SterennRD <s.reessedestoc@gmail.com>
2019-01-18 14:46:28 +0100

    Style du footer

--- footer.php
+++ footer.php
@@ -1,1 +1,21 @@
-<?php wp_footer(); ?>
+<footer class="footer">
+    <div class="container footer__container">
+        <div class="row">
+            <div class="col-lg-3">
+                <a href="<?php echo bloginfo('url'); ?>" class="footer__logo">
+                    Real<b>Home</b><span></span>
+                </a>
+
+                <?php wp_nav_menu(array( 'theme_location' => 'menu-secondaire','container' => 'nav', 'container_class' => 'footer__menu_rs')); ?>
+            </div>
+            <div class="col-lg-3">
+                <h3 class="footer__title">Menu</h3>
+                <?php wp_nav_menu(array( 'theme_location' => 'menu-principal','container' => 'nav', 'container_class' => 'footer__menu')); ?>
+            </div>
+
+
+        </div>
+
+    </div>
+    <?php wp_footer(); ?>
+</footer>
