9642f729aa73e33e2c232bdb58b5f0c1997e9280
SterennRD <s.reessedestoc@gmail.com>
2019-01-21 14:26:46 +0100

    Ajout du contact dans le footer

--- footer.php
+++ footer.php
@@ -12,6 +12,23 @@
                 <h3 class="footer__title">Menu</h3>
                 <?php wp_nav_menu(array( 'theme_location' => 'menu-principal','container' => 'nav', 'container_class' => 'footer__menu')); ?>
             </div>
+            <div class="col-lg-3 footer__contact">
+                <h3 class="footer__title">Contact us</h3>
+                <?php if(get_field('adresse', 74) && get_field('ville', 74)) :?>
+                    <div class="footer__contact_txt footer__contact_address"><?php the_field('adresse', 74); ?>, <?php the_field('ville', 74); ?></div>
+                <?php endif; ?>
+                <?php if(get_field('telephone')) :?>
+                    <div class="footer__contact_txt">Téléphone : <b><?php the_field('telephone'); ?></b></div>
+                <?php endif; ?>
+
+                <?php if(get_field('fax', 74)) :?>
+                    <div class="footer__contact_txt">FAX : <b><?php the_field('fax', 74); ?></b></div>
+                <?php endif; ?>
+
+                <?php if(get_field('email', 74)) :?>
+                    <div class="footer__contact_txt footer__contact_mail">E-Mail : <a href="mailto:<?php the_field('email', 74); ?>"><?php the_field('email', 74); ?></a></div>
+                <?php endif; ?>
+            </div>
 
 
         </div>
