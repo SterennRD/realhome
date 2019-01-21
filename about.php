e9d0f1d531d8348d3ed36ec5a0df12e957d3fe4e
SterennRD <s.reessedestoc@gmail.com>
2019-01-21 11:07:15 +0100

    Page à propos

---
+++ about.php
@@ -0,0 +1,48 @@
+<?php /* Template Name: Gabarit A propos */ ?>
+
+<?php get_header(); ?>
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div class="about">
+                    <div class="container">
+                        <h1 class="about__title">A <b>propos</b></h1>
+                        <div class="row about__article">
+                            <div class="col-lg-6">
+                                <?php if (get_the_post_thumbnail()) : ?>
+                                    <img class="about__img" src="<?php the_post_thumbnail_url('large'); ?>" />
+                                <?php endif; ?>
+                            </div>
+                            <div class="col-lg-6">
+                                <?php the_content(); ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php
+                    if( have_rows('features', 13) ):
+                        echo "<div class='features'><div class='container'><div class='row'>";
+                        while ( have_rows('features', 13) ) : the_row(); ?>
+                            <div class="features__block col-sm-12 col-md-6 col-lg-3">
+                                <div class="dashicons <?php the_sub_field('icon'); ?> features__icon"></div>
+                                <h3 class="features__title"><?php the_sub_field('titre-features'); ?></h3>
+                                <?php
+                                $features_txt = get_sub_field('texte-features');
+                                $features_txt = strip_tags($features_txt);
+                                ?>
+                                <p class="features__txt"><?php echo $features_txt; ?></p>
+                            </div>
+                        <?php endwhile;
+                        echo "</div></div></div>";
+                    endif;
+                    ?>
+
+                    <div class="container about__team">
+                        <h2>Notre <b>équipe</b></h2>
+                    </div>
+
+                </div>
+
+            <?php endwhile;?>
+        <?php endif;?>
+
+<?php get_footer(); ?>
