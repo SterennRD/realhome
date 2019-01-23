7a1c99d9d79c847cf94654e583b3252855f1f8eb
SterennRD <s.reessedestoc@gmail.com>
2019-01-23 16:28:10 +0100

    Style responsive du footer

--- footer.php
+++ footer.php
@@ -1,18 +1,18 @@
 <footer class="footer">
     <div class="container footer__container">
         <div class="row">
-            <div class="col-lg-3">
+            <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
                 <a href="<?php echo bloginfo('url'); ?>" class="footer__logo">
                     Real<b>Home</b><span></span>
                 </a>
 
                 <?php wp_nav_menu(array( 'theme_location' => 'menu-secondaire','container' => 'nav', 'container_class' => 'footer__menu_rs')); ?>
             </div>
-            <div class="col-lg-3">
+            <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
                 <h3 class="footer__title">Menu</h3>
                 <?php wp_nav_menu(array( 'theme_location' => 'menu-principal','container' => 'nav', 'container_class' => 'footer__menu')); ?>
             </div>
-            <div class="col-lg-3 footer__contact">
+            <div class="col-sm-12 col-md-4 col-lg-3 mb-3 footer__contact">
                 <h3 class="footer__title">Contact us</h3>
                 <?php if(get_field('adresse', 74) && get_field('ville', 74)) :?>
                     <div class="footer__contact_txt footer__contact_address"><?php the_field('adresse', 74); ?>, <?php the_field('ville', 74); ?></div>
