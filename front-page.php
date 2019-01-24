0787a3a8bd77876c5811c4d4a8fd94282e8cc182
SterennRD <s.reessedestoc@gmail.com>
2019-01-24 09:00:23 +0100

    Refactoring code page d'accueil

--- front-page.php
+++ front-page.php
@@ -17,10 +17,9 @@
                 </div>
             </section>
 
-            <?php
-            if( have_rows('features') ):
-                echo "<section class='features'><div class='container'><div class='row'>";
-                while ( have_rows('features') ) : the_row(); ?>
+            <?php if( have_rows('features') ): ?>
+                <section class='features'><div class='container'><div class='row'>
+                <?php while ( have_rows('features') ) : the_row(); ?>
                     <div class="features__block col-sm-12 col-md-6 col-lg-3">
                         <div class="fas fa-<?php the_sub_field('icon'); ?> features__icon"></div>
                         <h3 class="features__title"><?php the_sub_field('titre-features'); ?></h3>
@@ -30,20 +29,19 @@
                         ?>
                         <p class="features__txt"><?php echo $features_txt; ?></p>
                     </div>
-                <?php endwhile;
-                echo "</div></div></section>";
-            endif;
-            ?>
+                <?php endwhile; ?>
+                </div></div></section>
+            <?php endif; ?>
         <?php endwhile; ?>
     <?php endif; ?>
 
 <!--    AFFICHAGE DES DERNIERES PROPRIETES-->
-        <?php $Posts = get_the_ID(); ?>
         <?php
         $args = array(
             'post_type' => 'propriete',
             'posts_per_page' => 6,
             'order' => 'DESC',
+            'orderby' => 'ID',
         );
         ?>
 
@@ -53,75 +51,24 @@
             <section class="home__proprietes">
                 <div class="container">
                     <h2 class="home__proprietes_title">Nos <b>propriétés</b></h2>
-                    <p class="home__proprietes_txt">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p class="home__proprietes_txt">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
                     <div class="row">
                         <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                            <div class="col-sm-12 col-md-6 col-lg-4">
-                                <div class="properties__card">
-                                    <a href="<?php the_permalink() ?>">
-                                        <div class="properties__img <?php if (!has_post_thumbnail()) : ?>properties__img--empty<?php endif; ?>">
-                                            <?php if (has_post_thumbnail()) : ?>
-                                                <img src="<?php the_post_thumbnail_url('large') ?>" />
-                                            <?php endif; ?>
-                                        </div>
-                                        <div class="properties__desc">
-                                            <h2 class="properties__link">
-                                                <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
-                                            </h2>
-
-                                            <?php if (get_the_taxonomies() ) : ?>
-                                                <?php
-                                                $id = get_the_ID();
-                                                $terms = get_the_terms( $id, 'ville' );
-                                                if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-                                                    foreach ( $terms as $term ) {
-                                                        $term_link = get_term_link( $term, 'ville' );
-                                                        echo '<div class="properties__city"><a class="" href="' . $term_link . '">' . $term->name . '</a></div>';
-                                                    }
-                                                }
-                                                ?>
-                                            <?php endif ?>
-
-                                            <?php if(get_field('prix')): ?>
-                                                <?php $prix = get_field('prix');
-                                                $prix = number_format($prix, 2, ',', ' '); ?>
-                                                <div class="properties__price"><?php echo $prix ?> €</div>
-                                            <?php endif; ?>
-
-                                            <div class="properties__details">
-                                                <?php if(get_field('m2')): ?>
-                                                    <div><?php echo get_field('m2') ?>m²</div>
-                                                <?php endif; ?>
-
-                                                <?php if(get_field('nb_chambres')): ?>
-                                                    <div><?php echo get_field('nb_chambres') ?> chambres</div>
-                                                <?php endif; ?>
-
-                                                <?php if(get_field('nb_bain')): ?>
-                                                    <?php get_field('nb_bain') > 1 ? $salle = 'salles' : $salle = 'salle'; ?>
-                                                    <div><?php echo get_field('nb_bain') ?> <?php echo $salle; ?> de bain</div>
-                                                <?php endif; ?>
-                                            </div>
-                                        </div>
-
-
-
-
-
-                                    </a>
-                                </div>
-
-                            </div>
-
+                            <?php get_template_part( 'propriete-card' ); ?>
                         <?php endwhile; ?>
                     </div>
                     <div class="row justify-content-center mt-3">
-                        <a href="" class="button home__proprietes_button">Voir toutes</a>
+                        <a href="<?php bloginfo('url'); ?>/proprietes" class="button home__proprietes_button">
+                            Voir toutes
+                        </a>
                     </div>
                 </div>
             </section>
         <?php endif; ?>
-<!--    AFFICHAGE DES DERNIERES PROPRIETES-->
+
+<!--    AFFICHAGE DES MEMBRES DE L'EQUIPE-->
         <?php
         $args_team = array(
             'post_type' => 'equipe',
@@ -130,9 +77,9 @@
         );
         ?>
 
-        <?php $the_query = new WP_Query($args_team);?>
+        <?php $query_team = new WP_Query($args_team);?>
 
-        <?php if ( $the_query->have_posts() ) : ?>
+        <?php if ( $query_team->have_posts() ) : ?>
             <?php $nb = 0; ?>
             <section class="home__team">
                 <div class="container">
@@ -150,7 +97,7 @@
                         </div>
                         <div class="home__team_wrapper">
                             <div class="home__team_content">
-                                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                                <?php while ( $query_team->have_posts() ) : $query_team->the_post(); ?>
                                     <?php $nb++; ?>
                                     <div class="home__team_item home__team_item--<?php echo $nb; ?> row m-0 p-0">
                                         <div class="col-md-3 col-lg-4">
@@ -169,9 +116,7 @@
                                     </div>
                                 <?php endwhile; ?>
                             </div>
-
                         </div>
-
                     </div>
                 </div>
             </section>
@@ -182,19 +127,17 @@
 
 <!--    AFFICHAGE DES PARTENAIRES-->
             <?php ( have_rows( 'partenaires' ) ); ?>
-            <?php
-            if( have_rows('partenaires') ):
-                echo "<div class='container partners'><h2 class='partners__title'>Our <b>partners</b></h2>";
-                while ( have_rows('partenaires') ) : the_row();
-                    $img = get_sub_field('logo'); ?>
-                <div class="partners__logo">
-                    <img src="<?php echo $img['url']; ?>" alt="<?php if(get_sub_field('alt')): the_sub_field('alt'); endif;?>" title="<?php if(get_sub_field('alt')): the_sub_field('alt'); endif;?>"/>
+            <?php if( have_rows('partenaires') ): ?>
+                <div class='container partners'><h2 class='partners__title'>Our <b>partners</b></h2>
+                    <?php while ( have_rows('partenaires') ) : the_row();
+                        $img = get_sub_field('logo'); ?>
+                        <div class="partners__logo">
+                            <img src="<?php echo $img['url']; ?>" alt="<?php if(get_sub_field('alt')): the_sub_field('alt'); endif;?>" title="<?php if(get_sub_field('alt')): the_sub_field('alt'); endif;?>"/>
+                        </div>
+                    <?php endwhile; ?>
                 </div>
-                <?php endwhile;
-                echo "</div>";
-            endif;
-            ?>
-    <?php endwhile; ?>
+            <?php endif; ?>
+        <?php endwhile; ?>
     <?php endif; ?>
 
 <?php get_footer(); ?>
